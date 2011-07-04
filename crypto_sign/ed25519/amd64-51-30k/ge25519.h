c1261bd11e2ae526446c0bb44b2bb437da33a7c0
Daniel J. Bernstein <djb@cr.yp.to>
2011-07-04 12:11:22 -0500

    supercop-20110704

--- crypto_sign/ed25519/amd64-51-30k/ge25519.h
+++ crypto_sign/ed25519/amd64-51-30k/ge25519.h
@@ -22,12 +22,13 @@
 #define ge25519_double_scalarmult_vartime crypto_sign_ed25519_amd64_51_30k_batch_double_scalarmult_vartime
 #define ge25519_multi_scalarmult_vartime  crypto_sign_ed25519_amd64_51_30k_batch_ge25519_multi_scalarmult_vartime
 #define ge25519_scalarmult_base           crypto_sign_ed25519_amd64_51_30k_batch_scalarmult_base
-#define ge25519_p1p1_to_p2 crypto_sign_ed25519_amd64_51_30k_batch_ge25519_p1p1_to_p2
-#define ge25519_p1p1_to_p3 crypto_sign_ed25519_amd64_51_30k_batch_ge25519_p1p1_to_p3
-#define ge25519_add_p1p1   crypto_sign_ed25519_amd64_51_30k_batch_ge25519_add_p1p1
-#define ge25519_dbl_p1p1   crypto_sign_ed25519_amd64_51_30k_batch_ge25519_dbl_p1p1
-#define choose_t           crypto_sign_ed25519_amd64_51_30k_batch_choose_t
-#define ge25519_nielsadd2  crypto_sign_ed25519_amd64_51_30k_batch_ge25519_nielsadd2
+#define ge25519_p1p1_to_p2                crypto_sign_ed25519_amd64_51_30k_batch_ge25519_p1p1_to_p2
+#define ge25519_p1p1_to_p3                crypto_sign_ed25519_amd64_51_30k_batch_ge25519_p1p1_to_p3
+#define ge25519_add_p1p1                  crypto_sign_ed25519_amd64_51_30k_batch_ge25519_add_p1p1
+#define ge25519_dbl_p1p1                  crypto_sign_ed25519_amd64_51_30k_batch_ge25519_dbl_p1p1
+#define choose_t                          crypto_sign_ed25519_amd64_51_30k_batch_choose_t
+#define ge25519_nielsadd2                 crypto_sign_ed25519_amd64_51_30k_batch_ge25519_nielsadd2
+#define ge25519_nielsadd_p1p1             crypto_sign_ed25519_amd64_51_30k_batch_ge25519_nielsadd_p1p1
 
 #define ge25519_p3 ge25519
 
@@ -67,6 +68,7 @@ extern void ge25519_add_p1p1(ge25519_p1p1 *r, const ge25519_p3 *p, const ge25519
 extern void ge25519_dbl_p1p1(ge25519_p1p1 *r, const ge25519_p2 *p);
 extern void choose_t(ge25519_niels *t, unsigned long long pos, signed long long b, const ge25519_niels *base_multiples);
 extern void ge25519_nielsadd2(ge25519_p3 *r, const ge25519_niels *q);
+extern void ge25519_nielsadd_p1p1(ge25519_p1p1 *r, const ge25519_p3 *p, const ge25519_niels *q);
 
 extern const ge25519 ge25519_base;
 
@@ -80,7 +82,8 @@ extern void ge25519_add(ge25519 *r, const ge25519 *p, const ge25519 *q);
 
 extern void ge25519_double(ge25519 *r, const ge25519 *p);
 
-extern void ge25519_double_scalarmult_vartime(ge25519 *r, const ge25519 *p1, const sc25519 *s1, const ge25519 *p2, const sc25519 *s2);
+/* computes [s1]p1 + [s2]ge25519_base */
+extern void ge25519_double_scalarmult_vartime(ge25519 *r, const ge25519 *p1, const sc25519 *s1, const sc25519 *s2);
 
 extern void ge25519_multi_scalarmult_vartime(ge25519 *r, ge25519 *p, sc25519 *s, const unsigned long long npoints);
 
